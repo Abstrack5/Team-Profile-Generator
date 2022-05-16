86a7d95eef782c5ddad72736b6fe21892f79ab43
Abstrack <Armon_ah05@outlook.com>
2022-05-16 12:57:17 -0400

    finished

---
+++ src/role-template.js
@@ -0,0 +1,57 @@
+// const generateManagerCard = (manager) => {
+//     return `
+//       <div class="card employee-card">
+//           <div class="card-header bg-primary text-white">
+//               <h2 class="card-title">${manager.name}</h2>
+//               <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+//           </div>
+//           <div class="card-body">
+//               <ul class="list-group">
+//                   <li class="list-group-item">${manager.id}</li>
+//                   <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</a></li>
+//                   <li class="list-group-item">Office number: ${manager.officeNumber}</li>
+//               </ul>
+//           </div>
+//       </div>
+//       `;
+//   };
+  
+  
+//   const generateEngineerCard = (engineer) => {
+//     return `
+//       <div class="card employee-card">
+//           <div class="card-header bg-primary text-white">
+//               <h2 class="card-title">${engineer.name}</h2>
+//               <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+//           </div>
+//           <div class="card-body">
+//               <ul class="list-group">
+//                   <li class="list-group-item">ID: ${engineer.id}</li>
+//                   <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></li>
+//                   <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
+//               </ul>
+//           </div>
+//       </div>
+//       `;
+//   };
+  
+  
+//   const generateInternCard = (intern) => {
+//     return `
+//       <div class="card employee-card">
+//           <div class="card-header bg-primary text-white">
+//               <h2 class="card-title">${intern.name}</h2>
+//               <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+//           </div>
+//           <div class="card-body">
+//               <ul class="list-group">
+//                   <li class="list-group-item">ID: ${intern.id}</li>
+//                   <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
+//                   <li class="list-group-item">School: ${intern.school}</li>
+//               </ul>
+//           </div>
+//       </div>
+//       `;
+//   };
+
+
